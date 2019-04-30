1f087c31bf8273fa204c55365cc7f2aed7c65870
g.cheooool <g.cheooool@gmail.com>
2019-04-30 17:44:23 +0900

    dotenv 추가

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ boxes: [] });
     this.setState({ imageUrl: this.state.input });
-    fetch('https://ancient-depths-39946.herokuapp.com/imageurl', {
+    fetch('http://localhost:3000/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -110,7 +110,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://ancient-depths-39946.herokuapp.com/image', {
+          fetch('http://localhost:3000/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
