abe82fba3dcebd5c6f804d42ba38875088be3f14
g.cheooool <g.cheooool@gmail.com>
2019-05-01 20:02:07 +0900

    임시 컴포넌트
    - 기존 FaceRecognition 컴포넌트 리팩토링 예정

---
+++ src/components/FaceRecognition/FaceRecognitionContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class FaceRecognition extends Component {
+  render() {
+    return <div>FaceRecognition</div>;
+  }
+}
+
+export default FaceRecognition;
