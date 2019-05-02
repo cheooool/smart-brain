e2fff15d57d6ffc005bbec1428efd8d8ae65a597
g.cheooool <g.cheooool@gmail.com>
2019-05-02 18:27:48 +0900

    store 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'tachyons';
 import dotenv from 'dotenv';
+
+import { Provider } from 'react-redux';
+import store from './store';
 dotenv.config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
