632c2320d66e5ea5fb4f9d9da5aca6e621c980c7
g.cheooool <g.cheooool@gmail.com>
2019-05-02 14:18:50 +0900

    updated
    - faceRecognition 관련 메서드 제거
    - 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 import AuthContainer from './components/Auth/AuthContainer';
-import Signin from './components/Auth/Signin';
-import FaceRecognition from './components/FaceRecognition/FaceRecognitionContainer';
-
-// import Particles from 'react-particles-js';
-// import Navigation from './components/Navigation/Navigation';
-// import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-// import Logo from './components/Logo/Logo';
-// import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-// import Rank from './components/Rank/Rank';
+import FaceRecognitionContainer from './components/FaceRecognition/FaceRecognitionContainer';
 import './App.css';
 
 const GlobalStyle = createGlobalStyle`
@@ -39,30 +30,10 @@ const GlobalStyle = createGlobalStyle`
 //   }
 // };
 
-const initialState = {
-  input: '',
-  imageUrl: '',
-  box: {},
-  route: 'signin',
-  isSignedIn: false,
-  user: {
-    id: '',
-    name: '',
-    email: '',
-    entries: 0,
-    joined: ''
-  }
-};
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      boxes: [],
-      route: 'signin',
-      isSignedIn: false,
       user: {
         id: '',
         name: '',
@@ -85,86 +56,15 @@ class App extends Component {
     });
   };
 
-  getFaceLocations = data => {
-    const regions = data.outputs[0].data.regions;
-    return regions.map(region => {
-      return this.calculateFaceLocation(region);
+  updateUser = user => {
+    this.setState({
+      user
     });
   };
 
-  calculateFaceLocation = region => {
-    const clarifaiFace = region.region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    console.log(width, height);
-
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height
-    };
-  };
-
-  displayFaceBoxes = boxes => {
-    this.setState({ boxes });
-  };
-
-  onInputChange = event => {
-    this.setState({ input: event.target.value });
-  };
-
-  onButtonSubmit = () => {
-    this.setState({ boxes: [] });
-    this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        input: this.state.input
-      })
-    })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState({
-                user: {
-                  ...this.state.user,
-                  entries: count
-                }
-              });
-            })
-            .catch(console.log);
-        }
-        this.displayFaceBoxes(this.getFaceLocations(response));
-
-        // response.outputs[0].data.regions[0].region_info.bounding_box
-      })
-      .catch(err => console.log(err));
-  };
-
-  onRouteChange = route => {
-    if (route === 'signout') {
-      this.setState(initialState);
-    } else if (route === 'home') {
-      this.setState({ isSignedIn: true });
-    }
-    this.setState({ route: route });
-  };
-
   render() {
-    const { isSignedIn, imageUrl, route, boxes } = this.state;
-    console.log(this.state.user);
+    const { user } = this.state;
+
     return (
       <div className="App">
         <GlobalStyle />
@@ -184,37 +84,19 @@ class App extends Component {
           <PrivateRoute
             exact
             path="/"
-            component={FaceRecognition}
+            component={props => {
+              return (
+                <FaceRecognitionContainer
+                  {...props}
+                  user={user}
+                  updateUser={this.updateUser}
+                />
+              );
+            }}
             authenticated={this.state.user.id !== ''}
           />
         </Router>
         {/* <Particles className="particles" params={particlesOptions} /> */}
-        {/* <Navigation
-          isSignedIn={isSignedIn}
-          onRouteChange={this.onRouteChange}
-        /> */}
-        {/* {route === 'home' ? ( */}
-        {/* <> */}
-        {/* <Logo />
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
-            />
-
-            <FaceRecognition boxes={boxes} imageUrl={imageUrl} /> */}
-        {/* </> */}
-        {/* ) : route === 'signin' ? ( */}
-        {/* // <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} /> */}
-        {/* ) : ( */}
-        {/* // <Register
-          //   onRouteChange={this.onRouteChange}
-          //   loadUser={this.loadUser}
-          // /> */}
-        {/* // )} */}
       </div>
     );
   }
