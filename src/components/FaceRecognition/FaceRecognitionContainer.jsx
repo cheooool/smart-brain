f6f8fb0c0332dcf5951de50fb6e24abb676a5ad0
g.cheooool <g.cheooool@gmail.com>
2019-05-02 14:17:45 +0900

    스타일, 메서드 추가
    - Header, ImageLinkForm, FaceRecognition 컴포넌트 추가
    - App.js 내에 있던 faceRecognition 관련 메서드 가져옴

--- src/components/FaceRecognition/FaceRecognitionContainer.jsx
+++ src/components/FaceRecognition/FaceRecognitionContainer.jsx
@@ -1,9 +1,99 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Counter from '../Counter/Counter';
+import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './FaceRecognition';
 
-class FaceRecognition extends Component {
+class FaceRecognitionContainer extends Component {
+  state = {
+    boxes: [],
+    linkInput: '',
+    imageUrl: ''
+  };
+
+  getFaceLocations = data => {
+    const regions = data.outputs[0].data.regions;
+    return regions.map(region => {
+      return this.calculateFaceLocation(region);
+    });
+  };
+
+  calculateFaceLocation = region => {
+    const clarifaiFace = region.region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBoxes = boxes => {
+    this.setState({ boxes });
+  };
+
+  onInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  clearData = () => {
+    this.setState({ boxes: [] });
+    this.setState({ imageUrl: '' });
+  };
+
+  onDetectSubmit = async e => {
+    e.preventDefault();
+    this.clearData();
+    this.setState({
+      imageUrl: this.state.linkInput
+    });
+    const { user } = this.props;
+    const clarifaiResponse = await fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.linkInput
+      })
+    });
+    const clarifaiData = await clarifaiResponse.json();
+
+    if (clarifaiData) {
+      const entriesResponse = await fetch('http://localhost:3000/image', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id: user.id
+        })
+      });
+
+      const entriesData = entriesResponse.json();
+      console.log(entriesData);
+
+      this.displayFaceBoxed(this.getFaceLocations(clarifaiData));
+    }
+  };
+
   render() {
-    return <div>FaceRecognition</div>;
+    const { boxes, imageUrl } = this.state;
+    return (
+      <>
+        <Header />
+        {/* <Counter name={user.name} entries={user.entries} /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onDetectSubmit={this.onDetectSubmit}
+        />
+        <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+      </>
+    );
   }
 }
 
-export default FaceRecognition;
+export default FaceRecognitionContainer;
