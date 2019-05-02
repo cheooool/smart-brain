802c65f5f020fa7b569ee1d745caa5c28afdd1ac
g.cheooool <g.cheooool@gmail.com>
2019-05-02 13:12:24 +0900

    Header 추가

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../Logo/Logo';
+import Button from '../shared/Button';
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 1em;
+  box-shadow: -2px 3px 10px 2px rgba(0, 0, 0, 0.5);
+`;
+
+const SignoutButton = styled(Button)`
+  font-size: 0.75em;
+`;
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <SignoutButton type="button">Sign Out</SignoutButton>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
