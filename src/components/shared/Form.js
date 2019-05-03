797beb2797c8b09f7843c6b0000cef15d4719fdf
g.cheooool <g.cheooool@gmail.com>
2019-05-03 19:37:52 +0900

    form style 변경

--- src/components/shared/Form.js
+++ src/components/shared/Form.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const Form = styled.form`
-  max-width: 340px;
+  width: 100%;
   margin: 1em auto;
   padding: 2em;
+  box-sizing: border-box;
   box-shadow: 4px 4px 8px 4px rgba(0, 0, 0, 0.2);
 `;
 
